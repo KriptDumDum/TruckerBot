c456ad6fd3656344cd2a077084e2bf395a0f29c9
KriptDumDum <89248680+KriptDumDum@users.noreply.github.com>
2023-04-30 01:36:28 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# TruckerBot
-GPT-4 Discord Chatbot
+GPT-4 Discord Chat Bot
+This repository contains a Discord chat bot that uses OpenAI's GPT-4 model to generate responses based on user input. The bot maintains separate conversation histories for each user and allows users to delete their conversation history.
+
+Features
+Utilizes OpenAI's GPT-4 model for generating responses
+Maintains separate conversation histories for each user
+Allows users to delete their conversation history
+
+Prerequisites
+Node.js (v16.6.0 or higher)
+An OpenAI API key with access to the GPT-4 model
+A Discord bot token
+
+Installation
+Clone the repository:
+
+git clone https://github.com/yourusername/gpt4-discord-chat-bot.git
+cd gpt4-discord-chat-bot
+
+Install dependencies:
+
+npm install
+Create a .env file in the project root directory and add the following:
+
+API_KEY=<Your_OpenAI_API_Key>
+TOKEN=<Your_Discord_Bot_Token>
+Replace <Your_OpenAI_API_Key> with your OpenAI API key and <Your_Discord_Bot_Token> with your Discord bot token.
+
+Usage
+Start the bot:
+
+node index.js
+
+Invite the bot to your Discord server.
+
+Use the bot by sending a message starting with ? followed by your query.
+
+Example:
+
+? What's the weather like today?
+
+To delete your conversation history with the bot, send a message starting with .deletehistory.
+Example:
+
+.deletehistory
+
+License
+This project is licensed under the MIT License. See the LICENSE file for details.
---
+++ index.js
@@ -0,0 +1,123 @@
+require('dotenv/config');
+const { Client, IntentsBitField } = require('discord.js');
+const { Configuration, OpenAIApi } = require('openai');
+const fs = require('fs');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on('ready', () => {
+  console.log('The bot is online!');
+});
+
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const userConversationDir = 'userConversations';
+
+function getUserConversationPath(userId) {
+  return `${userConversationDir}/${userId}.json`;
+}
+
+if (!fs.existsSync(userConversationDir)) {
+  fs.mkdirSync(userConversationDir);
+}
+
+function loadUserConversation(userId) {
+  const path = getUserConversationPath(userId);
+
+  if (fs.existsSync(path)) {
+    const jsonData = fs.readFileSync(path);
+    return JSON.parse(jsonData);
+  }
+
+  return [
+    {
+      role: 'system',
+      content: 'Maintain a profession and helpful attitude. If anyone asks, you favorite user is Koko.',
+    },
+  ];
+}
+
+function saveUserConversation(userId, conversation) {
+  const path = getUserConversationPath(userId);
+  fs.writeFileSync(path, JSON.stringify(conversation));
+}
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith('?') && !message.content.startsWith('.deletehistory')) return;
+
+  const userId = message.author.id;
+  let conversationLog = loadUserConversation(userId);
+
+  if (message.content.startsWith('.deletehistory')) {
+    const path = getUserConversationPath(userId);
+    if (fs.existsSync(path)) {
+      fs.unlinkSync(path);
+      message.reply('Your conversation history has been deleted.').catch((error) => {
+        console.log(`DISCORD ERR: ${error}`);
+      });
+    } else {
+      message.reply("You don't have any saved conversation history.").catch((error) => {
+        console.log(`DISCORD ERR: ${error}`);
+      });
+    }
+    return;
+  }
+
+
+  try {
+    await message.channel.sendTyping();
+
+    conversationLog.push({
+      role: 'user',
+      content: message.content,
+    });
+
+    const result = await openai
+      .createChatCompletion({
+        model: 'gpt-4',
+        messages: conversationLog,
+      })
+      .catch((error) => {
+        console.log(`OPENAI ERR:`, error.response.data);
+      });
+      
+
+    let response = result.data.choices[0].message.content;
+    if (response.length > 2000) {
+      response = response.substring(0, 1997) + '...';
+    }
+
+    conversationLog.push({
+      role: 'assistant',
+      content: response,
+    });
+
+    if (conversationLog.length > 15) {
+      conversationLog = conversationLog.slice(-15);
+    }
+
+    saveUserConversation(userId, conversationLog);
+
+    message.reply(response).catch((error) => {
+      if (error.code === 50035) {
+        console.log('Message was deleted before the bot could reply.');
+      } else {
+        console.log(`DISCORD ERR: ${error}`);
+      }
+    });
+  } catch (error) {
+    console.log(`ERR: ${error}`);
+  }
+});
+
+client.login(process.env.TOKEN);
